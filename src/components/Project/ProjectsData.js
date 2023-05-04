a2ecc3f070e9709a70eabcd4cc899980470f76ed
YahuiG <36082743+YahuiG@users.noreply.github.com>
2023-05-04 23:35:10 +0800

    Update ProjectsData.js

--- src/components/Project/ProjectsData.js
+++ src/components/Project/ProjectsData.js
@@ -6,8 +6,7 @@ import cryptoapp from '../../images/cryptoapp2.png';
 const ProjectsData = [
   {
     title: 'Todo List App',
-    gitHub: 'https://github.com/wayungi/ToDoListApp',
-    live: 'https://soft-malabi-1a9e7c.netlify.app/',
+    live: 'https://cutetodolist.netlify.app/',
     description: [
       "A web app designed to keep track of complete and incomplete tasks.",
       "Users can add or delete tasks. Tasks can also be marked as complete or incomplete.",
@@ -19,7 +18,6 @@ const ProjectsData = [
 
   {
     title: 'Book Manager',
-    gitHub: 'https://github.com/wayungi/Awesome_books_es6_2',
     live: 'https://wayungi.github.io/Awesome_books_es6_2/',
     description: [
       "Awesome books is a SPA (Single Page Application) which stores books and their authors.",
@@ -28,29 +26,6 @@ const ProjectsData = [
     ],
     skills: ["JavaScript", "HTML", "CSS"],
     image: highground,
-  },
-  {
-    title: 'Crypto Cash',
-    gitHub: 'https://github.com/wayungi/crypto-cash',
-    live: 'https://diegoyon-crytoapp.netlify.app/',
-    description: [
-      "Cryto-cash is an application that consumes the CoinCap API that contains updated information about cryptocurrencies.",
-      "Currencies displayed can be filtered by ranking, price or percentage change. It also shows more information about each crytocurrency including a graph showing the price variations over time."
-    ],
-    skills: ['React', 'Redux'],
-    image: cryptoapp,
-  },
- 
-  {
-    title: 'Top Movies',
-    gitHub: 'https://github.com/wayungi/browse-movie',
-    live: 'https://alphantihinduka.github.io/Browse-movie-app/',
-    description: [
-      'A web application based on an external API which contains information about movies.',
-      'Users can like, comment on and reserve thier favourite movies. This applicaction uses an involvement API to save user interactions data.',
-    ],
-    skills: ['HTML', 'CSS', 'JavaScript', 'Webpack'],
-    image: shows,
   },
 ];
 
