3db4add10f904f3e27c5a7fb1ea81b97ec78712b
YahuiG <36082743+YahuiG@users.noreply.github.com>
2023-05-04 08:03:17 +0800

    Update About.js

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -9,17 +9,16 @@ const About = () => {
       <div className="information">
         <div className="bio" data-aos="flip-left">
           <p>
-            Hi! I am Francis, I love building new and innovative
+            Hi! I am Yahui, you can also call me Erica:)
+            I love building new and innovative
             solutions for the Web while solving all kinds of challenges
             that come along with the tasks.
           </p>
           <p>
-            I have  over 5 years of work experience and 2 of these are in software development.
-            I have worked as an IT technician, Scala back-end developer, website admin and as a DStv technician.
-            I am constantly expanding the limits of my technical abilities by learning new languages, frameworks, and technologies.
+            I have graduated from BCIT, majoring in Computer Systems Technology.
           </p>
           <p>
-            Reach out to me if you need a project built. Check out some of my work right below in the project section.
+            Reach out to me if you need a project design or test or any other help of coding. Check out some of my work right below in the project section.
           </p>
         </div>
         <div className="profile-pic" data-aos="flip-right">
